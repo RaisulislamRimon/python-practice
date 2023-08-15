59e7f113516f7e963c2d446c973d91f53de15f9c
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-15 19:09:41 +0600

    update

--- main.py
+++ main.py
@@ -486,6 +486,65 @@
 #
 #
 
+while True:
+    user_action = input('type add, show, complete, edit or exit: ')
+
+    match user_action:
+        case 'add':
+            todo = input('type your todo: ') + '\n'
+            # read files from older docuements
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            todos.append(todo)
+
+            # save file in txt document
+            file = open('todos.txt', 'w')
+            file.writelines(todos)
+            file.close()
+        case 'show':
+            # read files from older docuements
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
+            print('total items: ', len(todos))
+            if len(todos) == 0:
+                print('nothing...')
+
+            for index, item in enumerate(todos):
+                # print(index + 1, ". ", item)
+                print(f"{index + 1}.{item}")
+
+            for item in todos:
+                item = item.title()
+                # print(item)     # 1
+                                # 2
+                                # 3
+        case 'edit':
+            print('Got it')
+            number = int(input('Number of the todo to edit: '))
+            number = number - 1
+            print(number)
+            existing_todo = todos[number]
+            print('older value: ', existing_todo)
+            new_todo = input('enter new todo: ')
+            print('new value: ', new_todo)
+            todos[number] = new_todo
+
+        case 'complete':
+            number = int(input('number of the todo to complete: '))
+            todos.pop(number - 1)
+
+        case 'exit':
+            break
+        case _:
+            print('do not enter random input')
+
+print("bye")
+
+
 
 
 
