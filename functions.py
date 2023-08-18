23db93deaeff7318c318dc95764d777d085cc47e
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-19 00:28:18 +0600

    update

---
+++ functions.py
@@ -0,0 +1,54 @@
+# def get_todos(filepath):
+# def get_todos(filepath):   # TypeError: get_todos() missing 1 required positional argument: 'filepath'
+def get_todos(filepath='todos.txt'):
+    """ Read a text file and return the list of
+    to-do items. """
+    # filepath = 'todos.txt'
+    # with open('todos.txt', 'r') as file_local:
+    with open(filepath, 'r') as file_local:
+        # return filepath
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+# document the code #doc-strings
+# print(help(get_todos))
+# this is the result
+# Help on function get_todos in module __main__:
+#
+# get_todos(filepath='todos.txt')
+#     Read a text file and return the list of
+#     to-do items.
+#
+# None
+
+
+# def write_todos(filepath, todos_arg):
+# def write_todos(filepath='todos.txt', todos_arg):
+def write_todos(todos_arg, filepath='todos.txt'):
+    """ Write the to-do items list in the text file """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+
+# print(help(write_todos))
+
+# text = """
+# lorem ipsum     \
+# generated text  \
+# hello world     \
+# """
+
+# text = """
+# lorem ipsum     \n\
+# generated text \
+# hello world \
+# """
+
+text = """
+lorem ipsum
+generated text
+hello world
+"""
+
+# print(text)
