426e85d0c5699fe41582ea89f6f9805219e65ef1
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-21 18:04:04 +0600

    change the gui

--- exercises/exercise13.py
+++ exercises/exercise13.py
@@ -25,10 +25,12 @@ option1 = sg.Radio("Amphibians", group_id="question1")
 option2 = sg.Radio("Fish", group_id="question1")
 option3 = sg.Radio("Mammals", group_id="question1")
 option4 = sg.Radio("Birds", group_id="question1")
+quit_btn = sg.Button('Quit')
 
 window = sg.Window("File Compressor",
                    layout=[[label],
                            [option1], [option2], [option3], [option4],
+                           [quit_btn]
                            ])
 
 window.read()
