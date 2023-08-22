8e23e448b7e325a09aad849bf3c5874166d3ab26
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-22 22:18:08 +0600

    design the desktop app

---
+++ bonus/bonus_18_zip_extractor/zip_extractor.py
@@ -0,0 +1,30 @@
+import PySimpleGUI as sg
+
+sg.theme("Black")
+
+label1 = sg.Text("Select archive dir : ")
+input1 = sg.Input()
+choose_button1 = sg.FileBrowse("Choose", key="archive")
+
+label2 = sg.Text("Select destination: ")
+input2 = sg.Input()
+choose_button2 = sg.FileBrowse("Choose", key="folder")
+
+extract_button = sg.Button("Extract")
+output_label = sg.Text(key="output", text_color="green")
+
+layout = [
+    [label1, input1, choose_button1],
+    [label2, input2, choose_button2],
+    [extract_button, output_label]
+]
+
+window = sg.Window("Archive Extractor", layout)
+
+while True:
+    event, values = window.read()
+
+    if event == sg.WINDOW_CLOSED or event == 'Quit':
+        break
+
+window.close()
