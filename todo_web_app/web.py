9c662dbfd672b0ae73653106fd90e7d34d943b4f
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-23 15:58:07 +0600

    todo web app update

--- todo_web_app/web.py
+++ todo_web_app/web.py
@@ -1,17 +1,22 @@
 import streamlit as st
+# import streamlit as st
+import functions
 import os
 
 if not os.path.exists('todos.txt'):
     with open('todos.txt', 'w') as file:
         pass
 
-x = st.slider("Select a value")
-st.write(x, "squared is", x * x)
+# function call for get_todos
+todos = functions.get_todos()
 
-print(x)
 
-# import streamlit as st
-import functions
+def add_todo():
+    todo = st.session_state['new_todo']
+    todos.append(todo + '\n')
+    functions.write_todos(todos)
+    # print(todo)
+
 
 st.title('My Todo Web App')
 st.subheader('This is my todo web app')
@@ -19,14 +24,13 @@ st.write('This app is to increase your productivity')
 st.checkbox('Buy grocery')
 st.checkbox('Throw the trash')
 
-# function call for get_todos
-todos = functions.get_todos()
 for todo in todos:
     st.checkbox(todo)
-
-# st.text_input()
+    
+# st.text_input('')
 # st.text_input(label='')
 # st.text_input(label='Enter a todo: ')
-# st.text_input(label='Enter a todo: ', placeholder='Add new todo...')
+st.text_input(label='Enter a todo: ', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
 
 print('hello')
+st.session_state
