0eaa39f40f68d36fce81c8de5ede39744789029e
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2023-08-23 16:22:59 +0600

    todo web app update

--- todo_web_app/web.py
+++ todo_web_app/web.py
@@ -23,14 +23,22 @@ st.subheader('This is my todo web app')
 st.write('This app is to increase your productivity')
 st.checkbox('Buy grocery')
 st.checkbox('Throw the trash')
+# take input from the user
+st.text_input(label='Enter a todo: ', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
+st.write('If any task is complete, put a tik mark to delete that task.')
 
-for todo in todos:
-    st.checkbox(todo)
-    
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
 # st.text_input('')
 # st.text_input(label='')
 # st.text_input(label='Enter a todo: ')
-st.text_input(label='Enter a todo: ', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
+# st.text_input(label='Enter a todo: ', placeholder='Add new todo...', on_change=add_todo, key='new_todo')
 
-print('hello')
-st.session_state
+# print('hello')
+# st.session_state
